10963a7eb47656c21b6b8abe21b8b2f092e39728
Xavier Alexander <xalexander51@gmail.com>
2021-07-05 09:40:55 -0400

    Add car colors, and collision.

--- main_game.py
+++ main_game.py
@@ -1,10 +1,13 @@
+import turtle
 from turtle import Screen, Turtle
 from random import  randint
 import time
+turtle.colormode(255)
 #Screen
 screen = Screen()
 screen.tracer(0)
 screen.setup(700,600)
+screen.screensize(700,600)
 
 #Create The Turtle
 my_turtle = Turtle()
@@ -16,18 +19,19 @@ my_turtle.setposition(0,-200)
 car_list = []
 
 #Create The Blocks/Cars
-num = 5
-for i in range(10):
-	car = Turtle()
-	car.penup()
-	car.shape("square")
-	car.shapesize(0.8, 2)
-	car.setpos(randint(0,200), randint(0,100))
-	car.setheading(180)
-	car_list.append(car)
-print(car_list)
+def generate_cars():
+	for i in range(20):
+		car = Turtle()
+		car.penup()
+		car.shape("square")
+		car.shapesize(0.8, 2)
+		car.setpos(randint(350,800), randint(-500,500))
+		car.setheading(180)
+		car.fillcolor(randint(0, 255), randint(0, 255), randint(0, 255))
+		car_list.append(car)
 
 
+generate_cars()
 def car_move(car):
 	car.forward(8)
 
@@ -41,8 +45,17 @@ screen.onkey(up, "Up")
 game_on = True
 
 while game_on:
+	if car_list[-1].xcor() < 0:
+		print("OK")
+		for car in car_list:
+			car.clear()
+		generate_cars()
 	screen.update()
 	for car in car_list:
+		#Check For Collision
+		if my_turtle.distance(car) < 40:
+			game_on = False
+			print("Ouch")
 		car_move(car)
 	time.sleep(0.1)
 
