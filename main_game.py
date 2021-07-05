24aa3ddcdf8170d4554e4e2dddea1f97f5dd4bd7
Xavier Alexander <xalexander51@gmail.com>
2021-07-05 08:22:34 -0400

    Add multiple, moving cars

--- main_game.py
+++ main_game.py
@@ -1,31 +1,39 @@
-import turtle
 from turtle import Screen, Turtle
+from random import  randint
 import time
 #Screen
 screen = Screen()
 screen.tracer(0)
+screen.setup(700,600)
 
 #Create The Turtle
-turtle = Turtle()
-turtle.penup()
-turtle.shape("turtle")
-turtle.setheading(90)
+my_turtle = Turtle()
+my_turtle.penup()
+my_turtle.shape("turtle")
+my_turtle.setheading(90)
+my_turtle.setposition(0,-200)
 
+car_list = []
 
 #Create The Blocks/Cars
-car = Turtle()
-car.penup()
-car.shape("square")
-car.shapesize(0.8, 4)
-car.setpos(200,100)
-car.setheading(180)
+num = 5
+for i in range(10):
+	car = Turtle()
+	car.penup()
+	car.shape("square")
+	car.shapesize(0.8, 2)
+	car.setpos(randint(0,200), randint(0,100))
+	car.setheading(180)
+	car_list.append(car)
+print(car_list)
 
-def car_move():
-	car.forward(5)
+
+def car_move(car):
+	car.forward(8)
 
 #Control The Turtle
 def up():
-	turtle.forward(10)
+	my_turtle.forward(10)
 screen.listen()
 screen.onkey(up, "Up")
 
@@ -34,7 +42,8 @@ game_on = True
 
 while game_on:
 	screen.update()
-	car_move()
+	for car in car_list:
+		car_move(car)
 	time.sleep(0.1)
 
 screen.exitonclick()
